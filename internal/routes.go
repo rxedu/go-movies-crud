1fb2b6730c3bf9793c22738183490ac51ea5bdac
Evan Sosenko <razorx@evansosenko.com>
2022-09-01 22:33:01 -0500

    Add error checks

--- internal/routes.go
+++ internal/routes.go
@@ -2,15 +2,20 @@ package internal
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func GetMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
+	err := json.NewEncoder(w).Encode(movies)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func CreateMovie(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +24,10 @@ func CreateMovie(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&movie)
 	movie.ID = strconv.Itoa(rand.Intn(100000000))
 	movies = append(movies, movie)
-	json.NewEncoder(w).Encode(movie)
+	err := json.NewEncoder(w).Encode(movie)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func GetMovie(w http.ResponseWriter, r *http.Request) {
@@ -28,7 +36,10 @@ func GetMovie(w http.ResponseWriter, r *http.Request) {
 
 	for _, item := range movies {
 		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
+			err := json.NewEncoder(w).Encode(item)
+			if err != nil {
+				log.Fatal(err)
+			}
 			break
 		}
 	}
@@ -48,7 +59,10 @@ func UpdateMovie(w http.ResponseWriter, r *http.Request) {
 			movie.ID = ID
 
 			movies = append(movies, movie)
-			json.NewEncoder(w).Encode(movie)
+			err := json.NewEncoder(w).Encode(movie)
+			if err != nil {
+				log.Fatal(err)
+			}
 			break
 		}
 	}
@@ -61,7 +75,10 @@ func DeleteMovie(w http.ResponseWriter, r *http.Request) {
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
-			json.NewEncoder(w).Encode(item)
+			err := json.NewEncoder(w).Encode(item)
+			if err != nil {
+				log.Fatal(err)
+			}
 			break
 		}
 	}
